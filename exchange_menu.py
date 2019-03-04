9f1a43895c60e93f9274a2f4a8012e66916ebb4a
Andriy <skyba0andriy@gmail.com>
2019-03-04 19:57:20 +0200

    add requirement list

--- exchange_menu.py
+++ exchange_menu.py
@@ -1,12 +1,15 @@
 import requests
 import datetime
 
-API = "https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange"
+URL_API = "https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange"
+CURRENT = 'cc'
+VALUE = 'rate'
+CURRENT_COUNTRY = ['UAH', 'USD', 'EUR', 'PLN']
+DEFAULT_CURRENT = CURRENT_COUNTRY[0]
 
-
-def view_info(default_currency, currencies):
-    print("Default currency: {0}".format(default_currency))
-    print("API URL: {0}".format(API))
+def view_info(DEFAULT_CURRENT, currencies):
+    print("Default current: {0}".format(DEFAULT_CURRENT))
+    print("API URL: {0}".format(URL_API))
 
     currency_rates = get_currency(currencies)
     for currency in currencies:
@@ -14,31 +17,31 @@ def view_info(default_currency, currencies):
 
 
 def get_currency(currencies):
-    request = requests.get("{0}?json".format(API))
+    request = requests.get("{0}?json".format(URL_API))
     if request.status_code != 200:
         print("Service is temporarily unavailable.")
     currency_rates = {}.fromkeys(currencies, 1)
     for i in request.json():
-        if i['cc'] in currencies:
-            currency_rates[i['cc']] = (i['rate'])
+        if i[CURRENT] in currencies:
+            currency_rates[i[CURRENT]] = (i[VALUE])
     return currency_rates
 
 
-def get_currency_rate_per_day(currency, date):
+def currency_day(currency, date):
     if currency == 'UAH':
         return 1
     day = date.strftime('%Y%m%d')
-    request = requests.get("{0}?valcode={1}&date={2}&json".format(API, currency, day))
+    request = requests.get("{0}?valcode={1}&date={2}&json".format(URL_API, currency, day))
     if request.status_code != 200:
-        print("Service is temporarily unavailable.")
+        print("Error maybe API doesn`t work :(")
     for i in request.json():
-        if i['cc'] == currency:
-            return i['rate']
+        if i[CURRENT] == currency:
+            return i[VALUE]
 
 
 def default_currency(currencies):
     while True:
-        print("Please choose from the next values:")
+        print("Choose values:")
         for choice, currency in enumerate(currencies, 1):
             print("\t[{0}] - {1}.".format(choice, currency))
         user_input = input("--- ")
@@ -51,12 +54,10 @@ def default_currency(currencies):
         else:
             print("Please choose correct value.")
 
-def convert_currency(default_currency, currencies):
-    print("\n\nPlease enter sum of currency and currency for converting.")
-    print("Or press 'q' to exit.")
-    print("Available currency:")
+def convert_currency(DEFAULT_CURRENT, currencies):
+    print("'q' --- to exit.")
     print(" ".join(currencies))
-    print("Input format: [sum][currency]")
+    print("[sum][currency]")
 
     while True:
         user_input = input("--- ")
@@ -71,89 +72,79 @@ def convert_currency(default_currency, currencies):
                 elif i.isalpha():
                     currency += i
         else:
-            print("Incorrect input. Please try again.")
+            print("Please try again")
             continue
         if not currency:
-            print("Please see input format:")
-            print("Write : [sum] and current: [currency]")
+            print("Correct : [sum] and [currency]")
             continue
         elif currency not in currencies:
-            print("No currency")
+            print("Incorrect input")
             continue
         currency_sum = int("".join(currency_sum))
         currency_rates = get_currency(currencies)
-        result = currency_sum * currency_rates[currency] / (currency_rates[default_currency])
-        print("{0} {1} = {2} {3}\n".format(currency_sum, currency, result, default_currency))
+        result = currency_sum * currency_rates[currency] / (currency_rates[DEFAULT_CURRENT])
+        print("{0} {1} = {2:.2f} {3}\n".format(currency_sum, currency, result, DEFAULT_CURRENT))
 
 
 def view_history(currencies):
-    print("\n\nPlease enter currency and number of days (default 3).")
+    print("\nEnter current and day {USD 5} (default 7 days).")
     print("Available currency:")
     print(" ".join(currencies))
-    print("Currency,sum like that {USD 10}")
-    default_days = '3'
+    default_days = '7'
     while True:
-        user_input = input(">>> ")
+        user_input = input("--- ")
         if user_input == 'q':
             break
         if user_input.isalpha():
             if user_input not in currencies:
-                print("No such currency. Please try again.")
+                print("No such currency, try again.")
                 continue
             currency, days = user_input, default_days
         elif user_input.isdigit():
-            print("Incorrect input. Please try again.")
+            print("Your requests not good, try again.")
             continue
         else:
             currency, days =  user_input.split(" ",1)
         if not days.isdigit() or currency not in currencies:
-            print("Incorrect input. Please try again.")
+            print("Your requests not good, try again.")
             continue
         days = int(days)
         for i in range(days):
             day = datetime.date.today() - datetime.timedelta(i)
-            rate = get_currency_rate_per_day(currency, day)
+            rate = currency_day(currency, day)
             print("On {0} 1 {1} = {2} UAH".format(day, currency, rate))
-
-CURRENCT_COUNTRY = ['UAH', 'USD', 'EUR', 'PLN']
 
 
 def main_menu_system():
     print("\n---------- MAIN MENU EXCHANGE SYSTEM ----------")
-    print("\n1. Show general info.")
-    print("\n2. Set default currency.")
+    print("\n1. Show info.")
+    print("\n2. Show default currency.")
     print("\n3. Convert currency.")
-    print("\n4. Show exchange rate history for currency.")
+    print("\n4. Show history.")
     print("\nq. Quit.\n")
 
-
-def get_user_choice():
-    return input(">>> Press count: ")
-
-
-default_current = CURRENCT_COUNTRY[0]
-press_user = ''
+PRESS_USER = ''
 
 if __name__ == '__main__':
     main_menu_system()
-    while press_user != 'q':
-        press_user = get_user_choice()
+    while PRESS_USER != 'q':
+        PRESS_USER = input("--- Press count: ")
         main_menu_system()
-        if press_user == '1':
-            view_info(default_current, CURRENCT_COUNTRY)
-        elif press_user == '2':
-            default_currency = default_currency(CURRENCT_COUNTRY)
+        if PRESS_USER == '1':
+            view_info(DEFAULT_CURRENT, CURRENT_COUNTRY)
+        elif PRESS_USER == '2':
+            default_currency = default_currency(CURRENT_COUNTRY)
             main_menu_system()
-        elif press_user == '3':
-            convert_currency(default_current, CURRENCT_COUNTRY)
-            input("Please press enter to continue.")
+        elif PRESS_USER == '3':
+            convert_currency(DEFAULT_CURRENT, CURRENT_COUNTRY)
+            input("Press -> 'enter' to main menu")
             main_menu_system()
-        elif press_user == '4':
-            view_history(CURRENCT_COUNTRY)
-            input("Please press enter to continue.")
+        elif PRESS_USER == '4':
+            view_history(CURRENT_COUNTRY)
+            input("Press -> 'enter' to main menu")
             main_menu_system()
-        elif press_user == 'q':
-            print("\nThanks for visiting!\n")
+        elif PRESS_USER == 'q':
+            print("\nGood buy\n")
             break
         else:
-            print("\nI didn't understand that choice.\n")
+            print("\nIts not correct!\n")
