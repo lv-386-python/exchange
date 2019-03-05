71f02577d60b20caa867b11d725e039fa192fb55
lhalam <galamagal86@gmail.com>
2019-03-05 03:04:33 -0800

    litl fix

--- converter.py
+++ converter.py
@@ -1,61 +1,75 @@
-#!/usr/bin/env python
-import requests
 import argparse
-from datetime import datetime, date
-import calendar
 import json
+from datetime import datetime
+
+import requests
 
 parser = argparse.ArgumentParser(description='Converter')
-parser.add_argument('fc', type=str, help='Which currency you want to convert')
-parser.add_argument('tc', type=str, help='In which currency you want to convert')
+parser.add_argument('-fc', type=str, help='Which currency you want to convert')
+parser.add_argument('-tc', type=str, help='In which currency you want to convert')
 
-parser.add_argument('-set_default', '--default', type=str, help='Setting the default currency', default='UAH')
-parser.add_argument('-convert', '--convert', help='Performs converting the currencies', action="store_true")
-parser.add_argument('-history', '--history', help='Show history of the realtime exchange rate', action='store_true')
-parser.add_argument('-info', '--info', help='Show information about currencies', action='store_true')
-parser.add_argument('am', type=int, help='The amount you want to convert')
+parser.add_argument('-s', '--default', type=str, help='Setting the default currency', default='UAH')
+parser.add_argument('-c', '--convert', help='Performs converting the currencies', action="store_true")
+parser.add_argument('-hi', '--history', help='Show history of the realtime exchange rate', action='store_true')
+parser.add_argument('-i', '--info', help='Show information about currencies', action='store_true')
+parser.add_argument('-am', type=int, help='The amount you want to convert')
 
 args = parser.parse_args()
 
-
-api_base = r"https://www.alphavantage.coh/query?function=".rstrip("/")
-api_key = "YENYAV2LN1O6X72E"
+api_base = "https://www.alphavantage.coh/query?function="
+api_key = "API_KEY"
+try:
+    from local_settings import *
+except ImportError:
+    pass
 
 session = requests.Session()
 
 
 def convert(from_currency, to_currency, amount):
-
     from_currency.upper()
     to_currency.upper()
     function = "CURRENCY_EXCHANGE_RATE"
-    url = api_base+function+'&from_currency='+from_currency+'&to_currency='+to_currency+'&apikey='+api_key
+    # url = api_base + function + '&from_currency=' + from_currency + '&to_currency=' + to_currency + '&apikey=' + api_key
+    url = "{api_base}{function}&from_currency={from_currency}&to_currency={to_currency}&apikey={api_key}".format(
+        api_base=api_base,
+        function="CURRENCY_EXCHANGE_RATE",
+        from_currency=from_currency,
+        to_currency=to_currency,
+        api_key=api_key
+    )
     request = requests.Request("GET", url)
     prepared = request.prepare()
     response = session.send(prepared)
     json_exc = response.json()
-    return(round(float(float(json_exc['Realtime Currency Exchange Rate']['5. Exchange Rate'])*amount),3))
-
+    exchange_rate = json_exc.get('Realtime Currency Exchange Rate')
+    if not exchange_rate:
+        return None
+    exchange_rate = exchange_rate.get('5. Exchange Rate')
+    if not exchange_rate:
+        return None
+    return round(float(exchange_rate) * amount, 3)
+    # return (round(float(float(json_exc['Realtime Currency Exchange Rate']['5. Exchange Rate']) * amount), 3))
 
 
 def show_information(json_exc):
     timestamp = datetime.fromtimestamp(json_exc['meta']['timestamp'])
     return (timestamp.strftime('%Y-%m-%d %H:%M:%S'))
-    #my_date = date.today()
-    #calendar.day_name[my_date.weekday()
+    # my_date = date.today()
+    # calendar.day_name[my_date.weekday()
 
 
 def show_history(from_currency, to_currency):
     function = "FX_DAILY"
-    url = api_base+function+"&from_symbol="+from_currency.upper()+"&to_symbol="+to_currency.upper()+"&apikey="+api_key
+    url = api_base + function + "&from_symbol=" + from_currency.upper() + "&to_symbol=" + to_currency.upper() + "&apikey=" + api_key
     request = requests.Request("GET", url)
     prepared = request.prepare()
     response = session.send(prepared)
     json_exc = response.json()
     got_dict = dict(json.dumps(json_exc))
-    #time_series = (json_exc["Time Series FX (Daily)"]).keys()
-    #sorted_by_value = sorted(time_series.items(), key=lambda kv: kv[0])
-    #print(sorted_by_value[0:3])
+    # time_series = (json_exc["Time Series FX (Daily)"]).keys()
+    # sorted_by_value = sorted(time_series.items(), key=lambda kv: kv[0])
+    # print(sorted_by_value[0:3])
     print(got_dict)
 
 
@@ -64,15 +78,17 @@ def set_default_currency():
 
 
 if __name__ == '__main__':
-        if args.convert:
-            print(
-                "The result of conversion from {} to {} with amount {} is {}".format(args.fc, args.tc, args.am,
-                                                                                     convert(args.fc, args.tc,
-                                                                                             args.am)))
-        elif args.history:
-            print("The daily time series of the {} and {}, updated realtime".format(args.fc, args.tc,
-                                                                                    show_history(args.fc, args.tc)))
+    print(args)
+    if args.convert:
+        print(
+            "The result of conversion from {} to {} with amount {} is {}".format(args.fc, args.tc, args.am,
+                                                                                 convert(args.fc, args.tc,
+                                                                                         args.am)))
+    elif args.history:
+        print("The daily time series of the {} and {}, updated realtime".format(args.fc, args.tc,
+                                                                                show_history(args.fc, args.tc)))
+    #
+    # elif args.set_default:
+    #     print("")
 
-        elif args.set_default:
-            print("")
-
+# python converter.py -c -fc uah -tc usd -am 100
