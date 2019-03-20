9bf992a2e842fb505b30e426aae76747a7e2399c
MaksymGromyk <maksymgrom@gmail.com>
2019-03-20 11:46:53 +0200

    refactoring arparse part of app

--- commands.py
+++ commands.py
@@ -1,114 +1,117 @@
-from datetime import datetime, timedelta
+#!/usr/bin/env python
+"""This module provides menu with all commands tщ manipulate data"""
+
 import time
+from datetime import datetime, timedelta
+
 import requests
-import json
+
+from utils import get_key, set_key
 
 
-CORE_API = "https://api.exchangeratesapi.io/"
-CURRENCIES = ['GBP', 'USD', 'EUR', 'CAD']
+class ApiParser:
+    ''' class to parse and manipulate data from requested url'''
 
+    def __init__(self):
+        self.url = "https://api.exchangeratesapi.io/"
+        self.default_cur = get_key('DEFAULT_CUR')
+        self.currencies = get_key('CURRENCIES')
 
-def convert():
-	'''Command that will be used to converts currency.'''
-	print('<<<<<<Please choose currencies and amount from the following: \n'
-		'\n {}, {}, {}\n\n GBP is default one>>>>>>'.format(
-					*CURRENCIES[1:]))
-	answer = input(
-		'Input format: \n[quantity] [currency] for default currency\n'
-		'[quantity+currency] [currency] for converting custom currency\n>>>>>> ')
-	answer = answer.split()
-	quantity = answer[0]
-	to_exchange = answer[1].upper()
-	print(quantity[:3])
-	if quantity.isnumeric():
-		url_parse = f'{CORE_API}latest?base={CURRENCIES[0]}&symbols={to_exchange}'
-		request = requests.get(url_parse).json()
-		for v in request['rates'].items():
-			print(f'For today {quantity} {CURRENCIES[0]} costs {v[1]*int(quantity[:3])} {v[0]}')
-	else:
-		url_parse = f'{CORE_API}latest?base={quantity[-3:]}&symbols={to_exchange}'
-		request = requests.get(url_parse).json()
-		for v in request['rates'].items():
-			print(f'For today {quantity[:3]} {quantity[-3:]} costs {v[1]*int(quantity[:3])} {v[0]}')
+    def convert(self, answer=False):
+        '''Command that convert the currency.'''
+        if answer is False:
+            print('\n<<<<<<Please choose currencies and amount from the following:'
+                  '{}\nGBP is default one>>>>>>'.format(self.currencies))
+            answer = input(
+                'Input format: \n[quantity] [currency] for default currency\n'
+                '[quantity+currency] [currency] for converting custom currency\n>>>>>> ')
+        if answer == 'q':
+            quit()
+        answer = answer.split()
+        quantity = answer[0]
+        to_exchange = answer[1].upper()
+        q_currency = quantity[-3:]
+        if quantity.isnumeric():
+            url_parse = '{}latest?base={}&symbols={}'.format(
+                self.url,
+                self.default_cur,
+                to_exchange)
+            request = requests.get(url_parse).json()
+            for value in request['rates'].items():
+                amount = value[1] * int(quantity[:3])
+                print(f'\nFor today {quantity} {self.default_cur} costs {round(amount, 3)} {value[0]}')
+        else:
+            url_parse = f'{self.url}latest?base={q_currency}&symbols={to_exchange}'
+            request = requests.get(url_parse).json()
+            for value in request['rates'].items():
+                amount = value[1] * int(quantity[:3])
+                print(f'\nFor today {quantity[:3]} {q_currency} costs {round(amount, 3)} {value[0]}')
 
+    def show_info(self):
+        '''
+        Command showing the info about the default currency,
+        source url and current
+        currencies to work with.
+        '''
+        source_api = 'SOURCE: ' + self.url + '/latest'
+        currencies = self.currencies
+        url_parse = f'{self.url}latest?base={self.default_cur}&symbols={currencies}'
+        request = requests.get(url_parse).json()
+        print('\nDefault currency: ' + self.default_cur + '\n')
+        for value in request['rates'].items():
+            print(f'For today 1 {self.default_cur} costs {round(value[1], 3)} {value[0]}')
+        print(source_api)
 
-def show_info():
-	'''
-	Command showing the info about the default currency,
-	source url and current
-	currencies to work with.
-	Returns:
-			Nessesary info to start converting proccess.
-	'''
-	source_api = 'SOURCE: ' + CORE_API + '/latest'
-	currencies = ','.join(i for i in CURRENCIES[1:])
-	url_parse = f'{CORE_API}latest?base={CURRENCIES[0]}&symbols={currencies}'
-	request = requests.get(url_parse).json()
-	for v in request['rates'].items():
-		print(f'For today 1 {CURRENCIES[0]} costs {v[1]} {v[0]}')
-	print('\n')
-	print('Default currency: ' + CURRENCIES[0] + '\n')
-	print(source_api)
+    def set_default(self, answer):
+        '''
+        Command to set default currency
 
+        '''
+        print('\n<<<<<<The list of available currencies: {}\n{} is default one>>>>>>'.format(
+            self.currencies, self.default_cur))
+        future_default_key = answer.upper()
+        if future_default_key not in get_key('CURRENCIES'):
+            print('Choose the one from the list')
+            self.set_default()
+            if answer == 'q':
+                quit()
+        set_key('DEFAULT_CUR', future_default_key)
+        self.default_cur = future_default_key
+        print(f'\n<<<<<<From now your default currence is {self.default_cur}.')
 
-def show_history():
-	'''Command showing the history of changing the currency rate in given
-	period.
-	'''
-	print('<<<<<<Here is the list of possible currencies:\n {}, {}, {}, {}'.format(*CURRENCIES))
-	time.sleep(1)
-	default_currency = input('>>>>>>Please choose one to follow: ')
-	while True:
-		if default_currency not in CURRENCIES:
-			time.sleep(1)
-			default_currency = input(
-				'<<<<<<Please check you input one more time\n\n'
-				'>>>>>>Please choose one to follow: \n'
-				).upper()
-		else:
-			amount_of_days = input(
-				'>>>>>>Please input period [in days]: '
-				)
-			if amount_of_days.isnumeric():
-				break
-		continue
-	period = datetime.today() - timedelta(int(amount_of_days))
-	period = period.strftime('%Y-%m-%d')
-	available_currencies = ','.join(i for i in CURRENCIES)
-	url_parse = f'{CORE_API}{period}?base={default_currency}&symbols={available_currencies}'
-	request = requests.get(url_parse).json()
-	print(request)
-	for k, v in request['rates'].items():
-		print(f'{k} - {v}') 
+    def show_history(self):
+        '''Command showing the history of changing the currency rate in given
+        period.
+        '''
+        print('<<<<<< Here is the list of possible currencies: {}\n'.format(
+            ', '.join([c for c in self.currencies.split(',')])))
+        time.sleep(1)
+        default_currency = input('>>>>>>Please choose one to follow: \n>>>>>>')
+        while True:
+            if default_currency not in self.currencies.split(','):
+                time.sleep(1)
+                default_currency = input(
+                    '<<<<<<Please check you input one more time\n\n'
+                    '>>>>>>Please choose one to follow: \n'
+                ).upper()
+            else:
+                amount_of_days = input(
+                    '>>>>>>Please input period [in days]: ')
+                if amount_of_days.isnumeric():
+                    break
+            continue
 
-
-def set_default():
-	'''
-	Command to set default currency
-	Args:
-			param1: str, currency to set as default
-	Returns:
-			Info about changes that was done.
-	'''
-	default_currency = input(
-		'>>>>>>Plese input the currency that you want to setup as default\n>>>>>>')
-	default_currency = default_currency.upper()
-	while True:
-		if default_currency in CURRENCIES:
-			break
-		else:
-			default_currency = input(
-				'>>>>>>Please choose one from the list\n {}, {}, {}, {}\n>>>>>>'.format(
-					*CURRENCIES))
-	del CURRENCIES[0]
-	CURRENCIES.insert(0, default_currency)
-	print(f'<<<<<<From now your default currence is {default_currency}.')
-
-
-def good_bye():
-	'''
-	Command to quit the program and printing Good bye message.
-	''' 
-	good_bye_message = 'Thank you for using Exchange app. Take care.'
-	print(good_bye_message.center(get_terminal_size()[0], '*'))
-	sys.exit()
+        delta = int(amount_of_days)
+        url_parses = [
+            '{}{}?base={}&symbols={}'.format(
+                self.url,
+                (datetime.now() - timedelta(i)).strftime('%Y-%m-%d'),
+                self.default_cur,
+                self.currencies)
+            for i in range(1, delta + 1)]
+        responses = [requests.get(u).json() for u in url_parses]
+        for resp in responses:
+            print(f"For {resp['date']} rates were: \n")
+            for key, value in resp['rates'].items():
+                print(f'1 {self.default_cur} - {round(value, 3)} {key}')
+            print()
--- run.py
+++ run.py
@@ -1,22 +1,33 @@
-from commands import (
-				show_history, show_info, set_default, convert, good_bye)
+#!/usr/bin/env python
+'''Initialization module having menu launcher '''
 
-CURRENCIES = []
-COMMANDS = ['convert', 'help', 'history', 'set', 'q']
+import argparse
+from os import get_terminal_size
+from commands import ApiParser
+
+COMMANDS = ApiParser()
+PARSER = argparse.ArgumentParser(description='Simple console app for converting currencies.')
+PARSER.add_argument('-i', action='store_true',
+                    help='''Showing info about the default currency, source url and current
+                    currencies to work with''')
+PARSER.add_argument('-d', action='store', help='''Type currency to set as default''')
+PARSER.add_argument('-c', action='store', help=''''Input format: \n[quantity] [currency] for default currency\n'
+                    '[quantity+currency] [currency] for converting custom currency\n>>>>>> ''')
+
 
 def menu():
     '''Displaying the menu'''
     greating = (
-        '*********Hello there!*********\n'
+        '\n*********Hello there!*********\n'
         '***Welcome to Exchange app!***\n'
         'To use Exchange type one of the following commands:\n'
-        '* [convert]\n'
-        '* [help] for showing info\n'
-        '* [history] for showing rate history (Input format: [currency] [sum])\n'
-        '* [set] for setting default currency\n'
-        '* [q] for exit\n'
-                )
-    return greating 
+        '1) [convert]\n'
+        '2) [help] for showing info\n'
+        '3) [history] for showing rate history (Input format: [currency] [sum])\n'
+        '4) [set] for setting default currency\n'
+        '5) [q] for exit\n'
+    )
+    print(greating)
 
 
 def get_answer():
@@ -24,27 +35,40 @@ def get_answer():
     return input('......\nPlease input the command:\n>>>>>> ')
 
 
+def good_bye():
+    '''
+    Command to quit the program and printing Good bye message.
+    '''
+    good_bye_message = 'Thank you for using Exchange app. Take care.'
+    print(good_bye_message.center(get_terminal_size()[0], '*'))
+
+
 if __name__ == "__main__":
-    print(menu())
-    answer = ''
-    while answer != 'q':
-        answer = get_answer()
-        if answer == 'convert':
-            convert()
-        elif answer == 'help':
-            show_info()
-        elif answer == 'history':
-            show_history()
-        elif answer == 'set':
-            set_default()
-        elif answer == 'q':
-            good_bye()
-        else:
-            print('!!!\nPlease repeat carefully\n')
-            if get_answer() not in COMMANDS:
-                get_answer()
-        answer = input('Do you want to continue? [y/n]')
-        if answer == 'y':
+    ARGS = PARSER.parse_args()
+    if ARGS.i is True:
+        COMMANDS.show_info()
+    if ARGS.d:
+        COMMANDS.set_default(ARGS.d)
+    if ARGS.c:
+        COMMANDS.convert(answer=ARGS.c)
+    else:
+        while True:
             menu()
-        else:
-            good_bye()
+            answer = get_answer()
+            if answer in ['convert', '1']:
+                try:
+                    COMMANDS.convert()
+                except IndexError:
+                    COMMANDS.convert()
+            elif answer in ['help', '2']:
+                COMMANDS.show_info()
+            elif answer in ['history', '3']:
+                COMMANDS.show_history()
+            elif answer in ['set', '4']:
+                answer = input('>>>>>>Please input the currency that you want to setup as default\n>>>>>> ')
+                COMMANDS.set_default(answer)
+            elif answer == 'q':
+                good_bye()
+                break
+            else:
+                print('\n!!!\nНерпавильна комманда\n')
